b5217aa45a16d45ce251c7c581bad381f875386c
Chris M <therealmcafee88@gmail.com>
2020-08-11 21:42:47 -0400

    need changes

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from mainpage.models import Article, Author
 
-class ArticleForm(forms.Form):
+class AddAuthorForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    body = forms.CharField(widget=forms.Textarea)
+    author = forms.ModelChoiceField(queryset=Author.objects.all())
+
+
+class AddRecipeForm(forms.Form):
     title = forms.CharField(max_length=50)
     body = forms.CharField(widget=forms.Textarea)
     author = forms.ModelChoiceField(queryset=Author.objects.all())
